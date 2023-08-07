deb12a17aefbe719e5b1d17efb139e405d216c88
Usama Ahmad Jameel <34774596+samjzero019@users.noreply.github.com>
2023-08-07 18:18:21 +0500

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+set -e
+# Your script to run the Jupyter Notebook
+# jupyter nbconvert /home/ec2-user/SageMaker/llm-apps-workshop/blogs/rag/data_ingestion_to_vectordb.ipynb --execute
+echo "I am in script"
